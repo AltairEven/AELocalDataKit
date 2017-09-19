260fb70f1e707d466afbdb18da11283a7b10b525
qianye.qy <qianye.qy@alibaba-inc.com>
2017-09-19 14:52:37 +0800

    完善二级缓存同步

--- AELocalDataKit/Classes/BuiltinPlugs/AELDIntegratedCachePlug.m
+++ AELocalDataKit/Classes/BuiltinPlugs/AELDIntegratedCachePlug.m
@@ -39,16 +39,26 @@
     BOOL retValue = [memoryCachePlug startOperation:mode response:^(AELDResponse * _Nonnull resp) {
         operationResp = resp;
     }];
-    if (retValue && !operationResp.error) {
+    if (mode.operationType == AELDOperationTypeRead && retValue && operationResp.responseData) {
+        //如果是“读”操作，则在获取到数据后即返回
         if (response) {
             response(operationResp);
         }
         return YES;
     }
-    //如果内存缓存操作失败，则操作磁盘缓存
+    //如果内存缓存操作失败，或者是非“读”操作，则继续操作磁盘缓存
     retValue = [diskCachePlug startOperation:mode response:^(AELDResponse * _Nonnull resp) {
         operationResp = resp;
     }];
+    if (mode.operationType == AELDOperationTypeRead && retValue && operationResp.responseData) {
+        //进行到此处，说明磁盘中有缓存，但是内存中没有，则同步一份到内存
+        AELDOperationMode *syncMode = [AELDOperationMode modeWithName:mode.name operationType:AELDOperationTypeWrite];
+        syncMode.key = mode.key;
+        syncMode.value = operationResp.responseData;
+        [memoryCachePlug startOperation:syncMode response:^(AELDResponse * _Nonnull response) {
+            
+        }];
+    }
     if (response) {
         response(operationResp);
     }
